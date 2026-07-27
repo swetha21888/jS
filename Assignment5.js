bf547d13c01b5b47d22d47c098d38ec553b60fa2
Swetha B <swetha21888@gmail.com>
2026-07-27 16:50:44 +0530

    1. Simulated dice roll using Math.random(), Math.floor(), and +1 2. Created car object with name, model, and color properties 3. Accessed object property using dot notation 4. Built person object with name, age, and city 5. Updated city property and added new country property dynamically 6. Practiced object creation, property access, and modification.

---
+++ Assignment5.js
@@ -0,0 +1,28 @@
+//1
+console.log("Question 1");
+
+let dice=Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+//2
+console.log("Question 2");
+const car={
+    name:"Maruthi",
+    model:"Suzuki Dzire",
+    color:"Black"
+};
+console.log(car.name);
+
+//3
+console.log("Question 3");
+const person={
+    name:"Swetha",
+    age:23,
+    city:"Chennai"
+};
+console.log(person);
+console.log(person.city="New York");
+console.log(person);
+console.log(person.country="United States");
+console.log(person);
+
